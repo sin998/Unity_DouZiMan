9a1207b914de6072f0222ab3e8cd3f0afa192dfc
代志强 <710804343@qq.com>
2021-04-25 21:57:00 +0800

    实验一完成

---
+++ DouZiMan/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    public float force = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //Debug.Log("h:" + h);
+        rigidbody2D.AddForce(Vector2.right * h * force);
+    }
+}
