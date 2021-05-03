02ea1304d51dbbbabb6e6819c1d6696098e1eec9
代志强 <710804343@qq.com>
2021-05-03 19:56:26 +0800

    实验2完成

---
+++ DouZiMan/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    //相机：平滑过渡X
+    public float SmoothX = 5.0f;
+    //相机：平滑过渡Y
+    public float SmoothY = 5.0f;
+    //相机：摄像机里离hero的距离达到x后开始移动
+    public float xMargin = 2.0f;
+    //相机：摄像机里离hero的距离达到y后开始移动
+    public float yMargin = 2.0f;
+    //相机：Camera的XY的最小值：此值应该是在左上角，符号为负，XY值为相机实际的宽高的一半
+    public Vector2 MinCamXandY = new Vector2(-100.0f, -100.0f);
+    //相机：Camera的XY的最大值
+    public Vector2 MaxCamXandY = new Vector2(100.0f, 100.0f);
+
+    public Transform player;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TrackPlayer();
+    }
+    bool NeedMoveX()
+    {
+        return Mathf.Abs(transform.position.x - player.transform.position.x) > xMargin;
+    }
+    bool NeedMoveY()
+    {
+        return Mathf.Abs(transform.position.y - player.transform.position.y) > yMargin;
+    }
+    void TrackPlayer()
+    {
+        float fTargetX = transform.position.x;
+        float fTargety = transform.position.y;
+        if (NeedMoveX())
+        {
+            fTargetX = Mathf.Lerp(transform.position.x, player.transform.position.x,
+                                        Time.deltaTime * SmoothX);
+            fTargetX = Mathf.Clamp(fTargetX, MinCamXandY.x, MaxCamXandY.x);
+        }
+        if (NeedMoveY())
+        {
+            fTargety = Mathf.Lerp(transform.position.y, player.transform.position.y,
+                                        Time.deltaTime * SmoothY);
+            fTargety = Mathf.Clamp(fTargety, MinCamXandY.y, MaxCamXandY.y);
+        }
+
+        transform.position = new Vector3(fTargetX, fTargety, transform.position.z);
+    }
+}
--- DouZiMan/Assets/Scripts/PlayerControl.cs
+++ DouZiMan/Assets/Scripts/PlayerControl.cs
@@ -5,19 +5,118 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-    Rigidbody2D rigidbody2D;
-    public float force = 20;
+    //角色的刚体
+    private Rigidbody2D heroRD2D;
+    //角色的heroTransform
+    private Transform heroTransform;
+    //施加的力：移动
+    public float moveForce = 100;
+    //现在的朝向
+    private bool isFaceRight = true;
+    //移动：左右
+    private float h = 0.0f;
+    //最大速度
+    public float maxSpeed = 5;
+    //是否在地面上
+    private bool beGrounded = false;
+    //Ground的heroTransform
+    private Transform GroundCheckTf;
+    //跳跃力量的大小
+    public float JumpForce = 500;
+    //能否跳跃
+    private bool canJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        heroRD2D = this.GetComponent<Rigidbody2D>();
+        GroundCheckTf = heroTransform.Find("GroundCheck");
+    }
+    void Awake()
+    {
+        //heroTransform = GameObject.FindGameObjectWithTag("Player").transform; 
+        heroTransform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        //Debug.Log("h:" + h);
-        rigidbody2D.AddForce(Vector2.right * h * force);
+        //获取英雄的heroTransform，这里能用this是因为这个脚本就是挂在英雄上面的
+        heroTransform = this.gameObject.transform;
+
+        //获取水平方向的输入：AD/← →
+        h = Input.GetAxis("Horizontal");
+        //移动
+        move();
+
+        //跳跃
+        //射线检测
+        beGrounded = Physics2D.Linecast(heroTransform.position, GroundCheckTf.position, 1 << LayerMask.NameToLayer("Ground"));
+        //检测按键
+        if (beGrounded)
+        {
+            canJump = true;
+            //Debug.Log("在地上！");
+            if (Input.GetButtonDown("Jump"))
+            {
+                //Debug.Log("开始跳跃！");
+                jump();
+            }
+        }
     }
+
+    private void FixedUpdate()
+    {
+        //转身
+        //速度大于0（向右的力）并且现在朝向左，所以要转身
+        if (h > 0 && !isFaceRight)
+        {
+            flip();
+        }
+        //速度小于0（向左的力）并且现在朝向右，所以要转身
+        if (h < 0 && isFaceRight)
+        {
+            flip();
+        }
+    }
+
+    //跳跃
+    private void jump()
+    {
+        if (canJump)
+        {
+            Debug.Log("可以跳跃！");
+            heroRD2D.AddForce(new Vector2(0, 1) * JumpForce);
+            canJump = false;
+        }
+    }
+
+    //移动
+    private void move()
+    {
+        //控制移动
+        if (Mathf.Abs(heroRD2D.velocity.x) < maxSpeed)
+        {
+            heroRD2D.AddForce(Vector2.right * h * moveForce);
+        }
+        //控制最高速度
+        if (Mathf.Abs(heroRD2D.velocity.x) > maxSpeed)
+        {
+            heroRD2D.velocity = new Vector2(Mathf.Sign(heroRD2D.velocity.x) * maxSpeed, heroRD2D.velocity.y);
+        }
+    }
+
+    //转身
+    private void flip()
+    {
+        //改变标志位
+        isFaceRight = !isFaceRight;
+        //得到当前的转身
+        Vector3 v3LocalScale = heroTransform.localScale;
+        //转身
+        v3LocalScale.x *= -1;
+        //设置
+        heroTransform.localScale = v3LocalScale;
+    }
+
 }
